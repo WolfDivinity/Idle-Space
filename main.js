83ae2b79ffa9f86fa3a1f82c5d3557356f2aef96
Spencer Sandquist <talin763000@gmail.com>
2015-04-04 15:21:55 -0600

    First Commit
    No notes

---
+++ main.js
@@ -0,0 +1,49 @@
+var rocks =0;
+var miners =0;
+var money =0;
+function RockClick(number){
+rocks = rocks + number;
+document.getElementById("rocks").innerHTML = rocks;
+}
+function HireMiner(){
+    var minerCost = Math.floor(10 * Math.pow(1.1,miners));     //works out the cost of this cursor
+    if(rocks >= minerCost){                                   //checks that the player can afford the cursor
+        miners = miners + 1;                                   //increases number of cursors
+    	rocks = rocks - minerCost;                          //removes the cookies spent
+        document.getElementById('miners').innerHTML = miners;  //updates the number of cursors for the user
+        document.getElementById('rocks').innerHTML = rocks;  //updates the number of cookies for the user
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,miners));       //works out the cost of the next cursor
+    document.getElementById('minerscost').innerHTML = nextCost;  //updates the cursor cost for the user
+};
+function SellAllRocks(){
+var priceOfRocks = 10;
+if(rocks > 0){
+var amount = rocks * priceOfRocks;
+rocks =0;
+money = money+ amount;
+document.getElementById('money').innerHTML = money;
+document.getElementById('rocks').innerHTML = rocks;
+    }
+	else{
+	window.alert("No rocks to sell");
+	}
+}
+
+function Save(){
+var save = {
+rocks: rocks,
+miners: miners,
+money: money
+}
+localStorage.setItem("save",JSON.stringify(save));
+window.alert("Saved!");
+}
+function Load(){
+var savegame = JSON.parse(localStorage.getItem("save"));
+if (typeof savegame.rocks !== "undefined") rocks = savegame.rocks;
+if (typeof savegame.miners !== "undefined") miners = savegame.miners;
+if (typeof savegame.money !== "undefined") money = savegame.money;
+}
+
+window.setInterval(function(){RockClick(miners);},1000);
